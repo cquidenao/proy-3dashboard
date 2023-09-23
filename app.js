98c5a3031e5717d628b33df8abd3740e218cc307
cquidenao <c.a.p.quidenao@gmail.com>
2023-09-23 18:57:08 -0300

    comentando el app.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-// Importa los módulos necesarios de fetch.js
+// Importa los módulos necesarios desde fetch.js
 import { fetchExchangeRates } from './fetch.js';
 
 async function renderizarGrafico() {
     try {
+        // Obtiene los datos de tasas de cambio usando la función fetchExchangeRates
         const tasasCambio = await fetchExchangeRates();
 
+        // Extrae los símbolos de las monedas como etiquetas y los valores de las tasas de cambio
         const monedas = Object.keys(tasasCambio);
         const valoresTasasCambio = Object.values(tasasCambio);
 
+        // Obtiene el elemento canvas para renderizar el gráfico
         const ctx = document.getElementById("graficoTasasCambio");
 
+        // Crea un gráfico de barras utilizando Chart.js
         new Chart(ctx, {
             type: "bar",
             data: {
@@ -43,6 +47,7 @@ async function renderizarGrafico() {
             },
         });
     } catch (error) {
+        // Maneja los errores si la obtención o renderización del gráfico falla
         console.error("Error al obtener las tasas de cambio:", error);
     }
 }
